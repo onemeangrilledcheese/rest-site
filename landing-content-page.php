0f8d9268cb08bad6f6fc2ef681c8ead2f708c70d
Chris B <gossblackmon@gmail.com>
2014-11-11 20:14:56 -0600

    Finish media queries and intial prototyping of landing-page.

--- landing-content-page.php
+++ landing-content-page.php
@@ -8,23 +8,23 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="landing">
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+        <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
 
-    <div class="slider">
-        <?php echo do_shortcode("[metaslider id=46]"); ?>
-    </div>
+        <div class="slider">
+            <?php echo do_shortcode("[metaslider id=46]"); ?>
+        </div>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'restaurant-theme' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+        <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . __( 'Pages:', 'restaurant-theme' ),
+                    'after'  => '</div>',
+                ) );
+            ?>
+        </div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php edit_post_link( __( 'Edit', 'restaurant-theme' ), '<span class="edit-link">', '</span>' ); ?>
