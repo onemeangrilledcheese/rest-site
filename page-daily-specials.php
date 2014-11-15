231125c6007f974fc6b38595c769881f16dac888
Chris B <gossblackmon@gmail.com>
2014-11-14 22:41:54 -0600

    Update Daily Specials with ACF custom fields repeater. Set up to style.

--- page-daily-specials.php
+++ page-daily-specials.php
@@ -10,9 +10,43 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+            <?php
 
-				<?php // !!! NOTE: removed comments from page.php ?>
+            // check if the repeater field has rows of data
+            if( have_rows('create_new_daily_special') ):
+
+                // loop through the rows of data
+                while ( have_rows('create_new_daily_special') ) : the_row();
+
+                $image = get_sub_field('food_item_picture');
+
+                // image attributes
+                if( !empty($image) ):
+
+                    $size = 'thumbnail';
+                    $thumb = $image['sizes'][ $size ];
+                    $width = $image['sizes'][ $size . '-width' ];
+                    $height = $image['sizes'][ $size . '-height' ]; ?>
+
+	                <img src="<?php echo $thumb; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" />
+
+                <?php endif; ?>
+
+                <h2 class="food-name"><?php the_sub_field('food_item_name'); ?></h2>
+                <p class="food-price"><?php the_sub_field('food_item_price'); ?></p>
+                <p class="food-ingredient"><?php the_sub_field('food_item_ingredients'); ?></p>
+
+            <?php
+
+                endwhile;
+
+            else :
+
+                // no rows found
+
+            endif;
+
+            ?>
 
 			<?php endwhile; // end of the loop. ?>
 
