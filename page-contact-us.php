a190e5e918bc8872eb89df83cb4fd1f03f588473
Chris B <gossblackmon@gmail.com>
2014-11-30 00:37:31 -0600

    Complete all modules. Style navbar at various viewport widths. Hover effect to menu-items.

--- page-contact-us.php
+++ page-contact-us.php
@@ -5,12 +5,13 @@ Template Name: Contact Us
 
 get_header(); ?>
 
+<div class="inner-container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php get_template_part( 'contact-content', 'page' ); ?>
 
 				<?php // !!! NOTE: removed comments from page.php ?>
 
@@ -18,6 +19,6 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div><!-- .inner-container -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
